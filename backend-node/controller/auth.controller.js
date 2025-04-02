f0dac5f6beac089675df55627fc5653acf9374d8
aryansingh98875 <aryansingh98875@gmail.com>
2025-04-03 05:28:53 +0530

    added true in cors

--- backend-node/controller/auth.controller.js
+++ backend-node/controller/auth.controller.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 
 const COOKIE_OPTIONS = {
     httpOnly: true, // Prevent JavaScript access to the cookie
-    secure: false, // Use secure cookies in production
+    secure: true, // Use secure cookies in production
     sameSite: "None", // Prevent CSRF attacks
     maxAge: 24 * 60 * 60 * 1000 // 1 day
 };
